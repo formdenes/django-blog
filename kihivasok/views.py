28b5b736b1716993587e229562119dde6fcda6b7
sepp.akos <sepp.akos@mcssz.hu>
2020-03-30 17:07:31 +0200

    search finished - title, desc, tags works

--- kihivasok/views.py
+++ kihivasok/views.py
@@ -59,25 +59,67 @@ def challenge_create(request):
     return render(request, 'kihivasok/challenge_create.html', {'form': form})
 
 def kereses(request):
-    if request.method == "POST":
-        form = forms.SearchChallenge(request.POST)
-        if form.is_valid():
-            tags = form.cleaned_data['search_text']
-            tags = tags.split()
-            results = Challenge.objects.filter(tags__name__in=tags).distinct()
-            # title_results = Challenge.objects.filter(name='undefined')
-            # for t in tags:
-            #     q1 = Challenge.objects.filter(name__contains=tags.forloop.coun)
-            #     title_results.union(q1)
-            # title_results = Challenge.objects.filter(name__contains=tags[0])
-            # results += Challenge.objects.filter(tags__name__in=name).distinct()
-            return render(request,"search_challenge.html", {'form':form, 'results':results})
+    user = request.user
+    form_challenge = forms.AddChallengeToPatrol()
+    form_search = forms.SearchChallenge()
+    if user.is_authenticated:
+        patrol = Patrol.objects.get(group_leader=user)
+        patrol_id = patrol.id
+        if request.method == 'POST' and 'add' in request.POST:
+            form_challenge = AddChallengeToPatrol(request.POST)
+            if form_challenge.is_valid():
+                #addtolist
+                patrol_pk = form_challenge.cleaned_data['patrol']
+                patrol_inst = Patrol.objects.get(pk=patrol_pk)
+                challenge_pk = form_challenge.cleaned_data['challenge']
+                challenge_inst = Challenge.objects.get(pk=challenge_pk)
+                addChallenge = PatrolChallenge(
+                    patrol=patrol_inst, challenge=challenge_inst)
+                #check if already in list
+                try:
+                    addChallenge.save()
+                except:
+                    pass
+                return render(request, 'search_challenge.html', {'form_search':form_search, 'form_challenge':form_challenge})
+        elif request.method == "POST" and 'search' in request.POST:
+            form_search = forms.SearchChallenge(request.POST)
+            if form_search.is_valid():
+                tags = form_search.cleaned_data['search_text']
+                tags = tags.split()
+                # results = Challenge.objects.filter(tags__name__in=tags).distinct()
+                q_object = Q(name__icontains=tags[0])
+                for item in tags:
+                    q_object.add(Q(name__icontains=item) | Q(desc__icontains=item), Q.OR)
+                q_object.add(Q(tags__name__in=tags), Q.OR)
+                queryset = Challenge.objects.filter(q_object).distinct()
+                results = queryset
+                return render(request, "search_challenge.html", {'form_search': form_search, 'results': results, 'form_challenge':form_challenge})
+            else:
+                message = form_search.errors
+                return render(request, "search_challenge.html", {'form_search': form_search, 'message': message, 'form_challenge':form_challenge})
         else:
-            message= "Hibás keresés"
-            return render(request, "search_challenge.html",{'form':form, 'message':message})
+            form = AddChallengeToPatrol(initial={'patrol': patrol_id})
+        return render(request, 'search_challenge.html', {'form_search': form_search, 'form_challenge': form_challenge})
     else:
-        form = forms.SearchChallenge()
-    return render(request, "search_challenge.html", {'form':form})
+        if request.method == "POST":
+            form_search = forms.SearchChallenge(request.POST)
+            if form_search.is_valid():
+                tags = form_search.cleaned_data['search_text']
+                tags = tags.split()
+                results = Challenge.objects.filter(tags__name__in=tags).distinct()
+                # title_results = Challenge.objects.filter(name='undefined')
+                # for t in tags:
+                #     q1 = Challenge.objects.filter(name__contains=tags.forloop.coun)
+                #     title_results.union(q1)
+                # title_results = Challenge.objects.filter(name__contains=tags[0])
+                # results += Challenge.objects.filter(tags__name__in=name).distinct()
+                return render(request, "search_challenge.html", {'form_search': form_search, 'results': results})
+            else:
+                message = "Hibás keresés"
+                return render(request, "search_challenge.html", {'form_search': form_search, 'message': message})
+        else:
+            form_search = forms.SearchChallenge()
+    return render(request, "search_challenge.html", {'form_search':form_search})
 
 def index(request):
     actual_news = NewsPost.objects.filter(actual=True).order_by('-timestamp')
