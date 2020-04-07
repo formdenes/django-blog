62851dab0facb687983d405f44b8d9d9f3f7a2e3
sepp.akos <sepp.akos@mcssz.hu>
2020-04-07 13:18:05 +0200

    kihivasok egyedi oldalak created

--- kihivasok/urls.py
+++ kihivasok/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, re_path, path
 from . import views
+from .views import ChallengeDetailView
 
 app_name = 'challenge'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('create/', views.challenge_create, name='create'),
     path('mylist/', views.challenge_mylist, name='mylist'),
     path('list/', views.challenge_list, name='list'),
+    path('<slug:slug>', ChallengeDetailView.as_view(), name='challenge_detail'),
     path('tagsearch/', views.tag_search, name='tagsearch'),
     path('', views.challenge_list, name='challengelist'),
 ]
