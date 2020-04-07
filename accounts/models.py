5bc1074e1e75d0b6ac65be2b769e9ddfcbef9f18
sepp.akos <sepp.akos@mcssz.hu>
2020-04-07 09:31:22 +0200

    profile model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group_num = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.user.username , self.group_num)
